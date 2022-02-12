b31313ac75a215db6b7e3d2a637fc79b24c68330
gallaghj13 <gallagherj0913@gmail.com>
2022-02-12 12:57:52 -0700

    handleChange function

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -20,4 +20,12 @@ function Login(props) {
             console.log(err);
         }
     };
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
 }
