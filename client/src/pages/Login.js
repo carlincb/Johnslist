c3ef7ea205d68063d7a211f20ddc1723821e8e79
gallaghj13 <gallagherj0913@gmail.com>
2022-02-12 13:06:44 -0700

    first draft of form for login, class names not fleshed out

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -28,4 +28,44 @@ function Login(props) {
             [name]: value,
         });
     };
+
+    return (
+        <div className="">
+          <Link to="/signup">← Go to Signup</Link>
+  
+          <h2>Login</h2>
+          <form onSubmit={handleFormSubmit}>
+            <div className="">
+              <label htmlFor="email">Email address:</label>
+              <input
+                placeholder="youremail@test.com"
+                name="email"
+                type="email"
+                id="email"
+                onChange={handleChange}
+              />
+            </div>
+            <div className="">
+              <label htmlFor="pwd">Password:</label>
+              <input
+                placeholder="******"
+                name="password"
+                type="password"
+                id="pwd"
+                onChange={handleChange}
+              />
+            </div>
+            {error ? (
+              <div>
+                <p className="error-text">The provided credentials are incorrect</p>
+              </div>
+            ) : null}
+            <div className="">
+              <button type="submit">Submit</button>
+            </div>
+          </form>
+        </div>
+    )
 }
+
+export default Login;
