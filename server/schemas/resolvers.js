e258805818a85929fa89d2ac554a7f50021f96fd
Jack McNary <jack.mcnary@gmail.com>
2022-02-12 11:44:15 -0700

    building out resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,26 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Product, Category } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        user: async (parent, args, context) => {
+            if (context.user) {
+                const user = await User.findById(context.user.id).populate({
+                    path: 'orders.products',
+                    populate: 'category',
+                });
+
+                user.orders.sort((a, b) => b.purchaseDate - a.purchaseDate);
+
+                return user;
+            }
+
+            throw new AuthenticationError('Not logged in');
+        },
+
+
+    }
+
+
+}
