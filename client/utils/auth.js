e2353c684b11761dc81f86f4d206399548e63ff5
gallaghj13 <gallagherj0913@gmail.com>
2022-02-12 11:16:21 -0700

    added loggedIn and isTokenExpired functions

--- client/utils/auth.js
+++ client/utils/auth.js
@@ -4,6 +4,23 @@ class AuthService {
     getProfile() {
         return decode(this.getToken());
     }
+
+    loggedIn() {
+        const token = this.getToken();
+        return !!token && !this.isTokenExpired(token);
+    }
+
+    isTokenExpired(token) {
+        try {
+            const decoded = decode(token);
+            if (decoded.exp < Date.now() / 1000) {
+                return true;
+            } else return false;  
+        } catch (err) {
+            return false;
+        }
+    }
+    
 }
 
 export default new AuthService();
