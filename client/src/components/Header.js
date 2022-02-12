3f12e5f2e80c061db94bdfa5074c6653c6d3e75d
Zachary Elliott <zacharyberdell@gmail.com>
2022-02-12 14:03:32 -0700

    Started on the header.

---
+++ client/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header(props) {
+
+    return (
+        <header>
+            <a href="/"><img src="./images/kintsugi_logo.png" alt="Kintsugi Logo"/></a>
+
+            <nav>
+                <button></button>
+            </nav>
+        </header>
+    )
+}
