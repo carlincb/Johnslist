206ad3acf0761dce3dcc89459ef17e79e8f88f2b
gallaghj13 <gallagherj0913@gmail.com>
2022-02-12 11:20:35 -0700

    added getToken, login, and logout

--- client/utils/auth.js
+++ client/utils/auth.js
@@ -20,7 +20,20 @@ class AuthService {
             return false;
         }
     }
-    
+
+    getToken() {
+        return localStorage.getItem('id_token');
+    }
+
+    login(idToken) {
+        localStorage.setItem('id_token', idToken)
+        window.location.assign('/');
+    }
+
+    logout() {
+        localStorage.removeItem('id_token');
+        window.location.assign('/');
+    }
 }
 
 export default new AuthService();
