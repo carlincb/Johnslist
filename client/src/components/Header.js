470bdb039354ecacdf736916adcc6224f1e0db53
Zachary Elliott <zacharyberdell@gmail.com>
2022-02-13 12:34:21 -0700

    Working on array with information for dropdowns.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,12 +2,20 @@ import React from 'react';
 
 function Header(props) {
 
+    const dropdownMenus = [
+        {
+            title: "Sell",
+            linkInfo: [{href: "selling-info", }],
+            ariaLabel: "Open sell dropdown"
+        }
+    ]
+
     return (
         <header>
-            <a href="/"><img src="./images/kintsugi_logo.png" alt="Kintsugi Logo"/></a>
+            <a href="/" ariaLabel="Kintsugi home page" id="home-link"><img src="./images/kintsugi_logo.png" alt="Kintsugi home page"/></a>
 
-            <nav>
-                <button></button>
+            <nav id="sale-nav">
+                <button className="link-dropdown"></button>
             </nav>
         </header>
     )
