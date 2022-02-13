c7d1c45c397f43bd52e86178047a74205055eed0
Zachary Elliott <zacharyberdell@gmail.com>
2022-02-13 13:47:37 -0700

    Started building the map to create the dropdown menus.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,17 +5,29 @@ function Header(props) {
     const dropdownMenus = [
         {
             title: "Sell",
-            linkInfo: [{href: "selling-info", }],
-            ariaLabel: "Open sell dropdown"
-        }
+            linkInfo: [{href: "selling-info", text: "Information"}, {href: "marketplace", text: "Marketplace"}],
+            ariaLabel: "Open seller dropdown"
+        },
+        {
+            title: "Buy",
+            linkInfo: [{href: "buying-info", text: "Information"}, {href: "marketplace", text: "Marketplace"}],
+            ariaLabel: "Open buyer dropdown"
+        },
+       /* {
+            title: "Categories",
+            linkInfo: [...importedCategories],
+            ariaLabel: "Open category dropdown"
+        } */
     ]
 
     return (
         <header>
-            <a href="/" ariaLabel="Kintsugi home page" id="home-link"><img src="./images/kintsugi_logo.png" alt="Kintsugi home page"/></a>
+            <a href="/" id="home-link"><img src="./images/kintsugi_logo.png" alt="Go to Kintsugi home page"/></a>
 
             <nav id="sale-nav">
-                <button className="link-dropdown"></button>
+                {dropdownMenus.map(menu => {
+                    <button className="link-dropdown">{menu.title}<i></i></button>
+                })}
             </nav>
         </header>
     )
