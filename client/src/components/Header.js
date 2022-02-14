04d9e2ffff808ed13841c22bdb62f84a6949f83b
Zachary Elliott <zacharyberdell@gmail.com>
2022-02-14 09:50:37 -0700

    Comments added to document what the component is doing.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 function Header(props) {
+    //Toggles between light and dark themes
     const [ siteTheme, setSiteTheme ] = useState('./css/light.css');
+    //array the stores objects that are used to render the dropdown menus
     const dropdownMenus = [
         {
             title: "Sell",
@@ -13,17 +15,18 @@ function Header(props) {
             linkInfo: [{href: "buying-info", text: "Information"}, {href: "marketplace", text: "Marketplace"}],
             ariaLabel: "Open buyer dropdown"
         },
-       /* {
+        {
             title: "Categories",
             linkInfo: [...importedCategories],
             ariaLabel: "Open category dropdown"
-        } */
+        }
     ]
 
     return (
         <header>
             <a href="/" id="home-link"><img src="./images/kintsugi_logo.png" alt="Go to Kintsugi home page"/></a>
             <nav id="sale-nav">
+                {/* Creates a dropdown menu with it's links for all the objects in the dropdown array */}
                 {dropdownMenus.map(menu => {
                     <span class="dropdown-block">
                         <button className="link-dropdown" ariaLabel={menu.ariaLabel}>{menu.title}<i></i></button>
@@ -39,7 +42,10 @@ function Header(props) {
                 <a href="about">About Us</a>
                 <a href="login" id="login-link">Login</a>
             </nav>
-            <input type="checkbox" ariaLabel="Theme toggle button" id="them-toggle"/>
+            {/* Checkbox for toggling between light and dark themes */}
+            <input type="checkbox" ariaLabel="Theme toggle button" id="them-toggle" 
+            onChange={() => siteTheme === './css/light.css' ? 
+            setSiteTheme('./css/dark.css') : setSiteTheme('./css/light.css')}/>
         </header>
     )
 }
