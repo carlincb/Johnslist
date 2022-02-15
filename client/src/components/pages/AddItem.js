7f0e25d0819342d6b74b42c9355524f8612e70f9
Jack McNary <jack.mcnary@gmail.com>
2022-02-14 19:52:27 -0700

    Adding a basic form for the addItem page

--- client/src/components/pages/AddItem.js
+++ client/src/components/pages/AddItem.js
@@ -1,5 +1,70 @@
-import React, { useEffect } from 'react';
-import { useQuery } from '@apollo/client';
-import { QUERY_PRODUCTS } from '../../utils/queries';
-import { idbPromise } from '../../utils/helpers';
+import React from "react";
 
+
+export default function AddItemPage() {
+    const [product, setProduct] = React.useState('');
+    const [username, setUsername] = React.useState('');
+    const [image, setImage] = React.useState("");
+    const [description, setDescription] = React.useState("");
+    const [price, setPrice] = React.useState('');
+
+
+    const handleSubmit = (event) => {
+
+        event.preventDefault();
+
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <h1>Ready to add what could be someone's treasure?</h1>
+            <label>
+                Your Username:
+                <input
+                    name="username"
+                    type="username"
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
+                    required />
+            </label>
+            <label>
+                Product Name:
+                <input
+                    name="product"
+                    type="product"
+                    value={product}
+                    onChange={e => setProduct(e.target.value)}
+                    required />
+            </label>
+            <label>
+                Image:
+                {/* This is where we need to add that image thing from the previous project */}
+                <input
+                    name="image"
+                    value={image}
+                    onChange={e => setImage(e.target.value)}
+                    required
+                />
+            </label>
+            <label>
+                Description:
+                <input
+                    name="description"
+                    value={description}
+                    onChange={e => setDescription(e.target.value)}
+                    required
+                />
+            </label>
+            <label>
+                <input
+                    name="price"
+                    type="price"
+                    value={price}
+                    onChange={e => setPrice(e.target.value)}
+                    required />
+            </label>
+
+            <button>Submit</button>
+        </form>
+    );
+}
