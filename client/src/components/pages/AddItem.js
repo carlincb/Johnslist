cb48b2f14323179091615334e0c5c9b68ee00305
Jack McNary <jack.mcnary@gmail.com>
2022-02-14 20:15:28 -0700

    adding more add-items page

--- client/src/components/pages/AddItem.js
+++ client/src/components/pages/AddItem.js
@@ -7,7 +7,7 @@ export default function AddItemPage() {
     const [image, setImage] = React.useState("");
     const [description, setDescription] = React.useState("");
     const [price, setPrice] = React.useState('');
-
+    const [category, setCategory] = React.useState('');
 
     const handleSubmit = (event) => {
 
@@ -16,13 +16,28 @@ export default function AddItemPage() {
     }
 
     return (
+        // select category 
         <form onSubmit={handleSubmit}>
             <h1>Ready to add what could be someone's treasure?</h1>
+            <label>
+                Select the category you feel this fits in:
+                <select
+                    name="category"
+                    value={category}
+                    onChange={e => setCategory(e.target.value)}
+                    required />
+                <option key=""></option>
+                {categories.map(category => (
+                    <option key={category}>{category}</option>
+                ))}
+            </label>
+
+
             <label>
                 Your Username:
                 <input
                     name="username"
-                    type="username"
+                    type="text"
                     value={username}
                     onChange={e => setUsername(e.target.value)}
                     required />
@@ -31,7 +46,7 @@ export default function AddItemPage() {
                 Product Name:
                 <input
                     name="product"
-                    type="product"
+                    type="text"
                     value={product}
                     onChange={e => setProduct(e.target.value)}
                     required />
@@ -41,6 +56,7 @@ export default function AddItemPage() {
                 {/* This is where we need to add that image thing from the previous project */}
                 <input
                     name="image"
+                    type="input"
                     value={image}
                     onChange={e => setImage(e.target.value)}
                     required
@@ -50,6 +66,7 @@ export default function AddItemPage() {
                 Description:
                 <input
                     name="description"
+                    type="text"
                     value={description}
                     onChange={e => setDescription(e.target.value)}
                     required
@@ -58,13 +75,13 @@ export default function AddItemPage() {
             <label>
                 <input
                     name="price"
-                    type="price"
+                    type="text"
                     value={price}
                     onChange={e => setPrice(e.target.value)}
                     required />
             </label>
 
             <button>Submit</button>
-        </form>
+        </form >
     );
 }
