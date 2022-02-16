0838dfc0e3e1ec3f70d91d793a7892e74be1a1fb
Zachary Elliott <zacharyberdell@gmail.com>
2022-02-15 19:11:49 -0700

    Basics of the product-details page added.

--- client/src/pages/ProductDetails.js
+++ client/src/pages/ProductDetails.js
@@ -1,13 +1,20 @@
 import React, { useState } from 'react';
+import { useQuery, useMutation } from '@apollo/client';
 
 function ProductDetails(props) {
     return (
         <main>
             <img src={product.image} />
             <div className="flex column">
-                <div className="card">
-
+                <div className="card flex column">
+                    <h1>Product Details</h1>
+                    <h2>{product.productName}</h2>
+                    <p>{product.description}</p>
                 </div>
+                <a id="add-link" class=""
+                href={user.isSeller ? '/whatever-seller-link' : '/whatever-buyer-link'} >
+                    {user.isSeller ? 'Add to Gallery' : 'Add to Cart'}
+                </a>
             </div>
         </main>
     );
