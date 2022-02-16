917b9500dfc3dbc739595c0f19736e966b0f1157
carlincb <carlin.colleen@gmail.com>
2022-02-15 21:22:40 -0700

    Finished cloudinaryService.js.

--- client/src/utils/cloudinaryService.js
+++ client/src/utils/cloudinaryService.js
@@ -5,3 +5,26 @@ export const url = (publicId, options) => {
   const cloudinary = CoreCloudinary.new();
   return cloudinary.url(publicId, cloudinaryOptions);
 };
+
+export const uploadWidget = (options, callback) => {
+  const cloudinaryOptions = Util.withSnakeCaseKeys(options);
+  window.cloudinary.openUploadWidget(cloudinaryOptions, callback);
+};
+
+export const fetchPhotos = async (imageTag, setter) => {
+  const options = {
+    cloudName: "",
+    format: "json",
+    type: "list",
+    version: Math.ceil(new Date().getTime() / 1000),
+  };
+  const urlPath = url(imageTag.toString(), options);
+  fetch(urlPath)
+    .then((res) => res.text())
+    .then((text) =>
+      text
+        ? setter(JSON.parse(text).resources.map((image) => image.public_id))
+        : []
+    )
+    .catch((error) => console.log(error));
+};
