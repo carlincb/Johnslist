18d0c55b609692b7ebadb9b06ec3b451e7332100
gallaghj13 <gallagherj0913@gmail.com>
2022-02-16 18:47:35 -0700

    change to file structure and an import in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import {
   createHttpLink,
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
-import Header from './components/Header';
+import Header from './components/Header/Header';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
