965b01e256065a810e6f3a0736a4c35f673caf58
carlincb <carlin.colleen@gmail.com>
2022-02-19 14:50:19 -0700

    Initial homepage styling done.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,19 +1,42 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  
   return (
     <div>
-        <div>
-            <h2>The NEW commerce platform for broken items</h2>
-            <p>"One man's trash is another man's treasure."</p>
-            <p>~Leonhard Culmans 1741</p>
-        </div>
-        <div>
+      <section className="wrapper">
+        <div className="banner">
+          <div className="header">
+            <h1 className="title">
+              The NEW commerce
+              <br />
+              platform for
+              <br />
+              broken items
+            </h1>
+            <p className="subtitle">
+              "One man's trash is another man's treasure."
+            </p>
+            <p className="subtitle">~Leonhard Culmans 1741</p>
             <Link to="/signup">Start Today</Link>
+          </div>
+          <div className="profile"></div>
         </div>
+      </section>
     </div>
+    // <div>
+    //   <section className="wrapper">
+    //     <div className="banner">
+    //     <div className="header">
+    //         <h2 className="title">The NEW commerce platform for broken items</h2>
+    //         <p className="subtitle">"One man's trash is another man's treasure."</p>
+    //         <p className="subtitle">~Leonhard Culmans 1741</p>
+    //         <Link to="/signup">Start Today</Link>
+    //       </div>
+    //       <div className="profile"></div>
+    //     </div>
+    //   </section>
+    // </div>
   );
 };
 
