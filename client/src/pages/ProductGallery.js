5ca3ab460b27187e01e069ecc9eedc53997ccb3b
gallaghj13 <gallagherj0913@gmail.com>
2022-02-23 20:21:08 -0700

    getting ann add to wishlist to work

--- client/src/pages/ProductGallery.js
+++ client/src/pages/ProductGallery.js
@@ -1,11 +1,32 @@
 import React, { useState } from 'react';
-import { useQuery } from '@apollo/client';
+import { useQuery, useMutation } from '@apollo/client';
 import { QUERY_PRODUCTS } from '../utils/queries';
+import { ADD_WISH } from '../utils/mutations';
+import Auth from '../utils/auth';
 import './myproducts.css';
 
 const ProductGallery = () => {
     const { loading, data } = useQuery(QUERY_PRODUCTS);
     const productData = data?.products || [];
+
+    const [addWish, { error }] = useMutation(ADD_WISH);
+
+    const handleWish = async (productId) => {
+        
+        const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+        if (!token) {
+            return false;
+        }
+
+        try {
+            const { data } = await addWish({
+                variables: { _id: productId }
+            });
+        } catch (err) {
+            console.error(err);
+        };
+    };
 
     return (
         <div className='productCards'>
@@ -18,6 +39,8 @@ const ProductGallery = () => {
                             {product.image ? <img src={product.image} alt={`this is the product image`} /> : null}
                             <div>{product.name}</div>
                             <div>{product.description}</div>
+                            <div>{product.price}</div>
+                            <button onClick={() => handleWish(product._id)}>Add to Wishlist</button>
                         </div>
 
                     ))
