08bb7f825f2e7753d84a85e4cb71af1fb9a81e6f
zachary-berdell-elliott <zacharyberdell@gmail.com>
2022-03-14 10:51:07 -0600

    Attempting fix on product details css

--- client/src/components/ProductDetails/ProductDetailsBuyer.js
+++ client/src/components/ProductDetails/ProductDetailsBuyer.js
@@ -59,7 +59,7 @@ function ProductDetailsBuyer(props) {
 
     return (
         <main className="flex">
-            <link rel="stylesheet" href="/css/buyPage.css" />
+            <link rel="stylesheet" type="text/css" href="/css/buyPage.css" />
             <img src={productData.image} />
             <div className="flex column">
                 <div className="card flex column">
